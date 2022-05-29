6d629237922ece7cfde2dbbb0c9ce0d351a2512a
Jeong Mi Park <parkjeongmi0227@gmail.com>
2022-05-28 23:44:28 -0400

    Introduce Hash tables and hash function

---
+++ HashTables/HashTables_intro.js
@@ -0,0 +1,12 @@
+// Hash tables or hash maps, maps, unordered maps, dictionaries, object there are many ways to call
+// objects in JS are a type of hash table.
+// Python :  dictionaries
+// JS : objects
+// Java : maps
+// Ruby : hashes
+
+// A hash function is simply a function that generates a value of fixed length for each input that it gets.
+// idempotent :  a function given an input always outputs the same output.
+// we have a key and send it through a hash function that is going to 
+// hash something really fas and then map the hash that came outto be 
+// into a memory address where we want to store data. 
