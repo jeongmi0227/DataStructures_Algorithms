8f1f85c6ca44fc0738c6e7938d0f7402a25168d9
Jeong Mi Park <parkjeongmi0227@gmail.com>
2022-06-03 01:15:02 -0400

    Implement a Hash table

--- HashTables/HashTables_intro.js
+++ HashTables/HashTables_intro.js
@@ -29,8 +29,52 @@ console.log(user.age); // O(1)
 user.scream(); // O(1)
 // main problem with hash problem
 // Hash Collision :  computer has limited space and when we create an object or a hash table, the computer decide how much space to allocate. 
-// It's not going to allocate all the space to the hash table. It's going to allocate a bit of it. The hash function randomly assigned a space in memory and put it in.
+// It's not going to allocate all the space to the hash table. It's going to allocate a bit of it. The hash function randomly assigned a space in memory and put it in. 
+// * Hash function will not evenly distribute until the memory space is full, although hash functions are optimized to try to distribute data.
 // It slows down reading and writing with a hash table with O and divided by K
 // O(n/k) K is the size of the hash table
 // O(n/k) => O(n)
 // Occasionally depending on the hash function it might take O(n)
+
+// Q1) Implement A Hash table
+class HashTable{
+    constructor(size) {
+        this.data = new Array(size);
+    }
+
+    set(key, value) {
+        let address = this._hash(key);
+        if (!this.data[address]) {
+            this.data[address] = [];
+        } 
+        this.data[address].push([key, value]);
+        return this.data;
+    }
+
+    get(key) {
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        if (currentBucket) {
+            for (let i = 0; i < currentBucket.length; i++){
+                if (currentBucket[i][0] === key) {
+                    return currentBucket[i][1];
+                }
+            }
+        }
+        return undefined;
+    }
+    // put _ means private (developer standard)
+    _hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++){
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+        }
+        return hash;
+    }
+}
+
+const myHashTable = new HashTable(2);
+console.log(myHashTable.set('grapes', 10000));
+console.log(myHashTable.set('apples', 50000));
+
+console.log(myHashTable.get('grapes'));
