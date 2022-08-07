b329bd772fe17e0ddf68279130605788f94f02b4
Jeong Mi Park <parkjeongmi0227@gmail.com>
2022-08-07 01:05:50 -0400

    Implemented SelectionSort

--- Algorithm/Recursion/Sorting_intro.js
+++ Algorithm/Recursion/Sorting_intro.js
@@ -9,7 +9,7 @@
 // sort()method sort items according to string Unicode 
 // The time and space complexity of the sort cannot be guaranteed as it is implementation dependent.
 const num = [2, 6, 1, 33, 656, 32];
-console.log(num.sort());
+// console.log(num.sort());
 
 let bubble = [6, 5, 3, 1, 8, 7, 2, 4];
 //  5 3 1 6 7 2 4 8
@@ -28,21 +28,34 @@ function bubbleSort() {
     }
 }
 
-console.log('Before bubbleSort');
-console.log(bubble);
+// console.log('Before bubbleSort');
+// console.log(bubble);
 
-bubbleSort(bubble);
+// bubbleSort(bubble);
 
-console.log('After bubbleSort');
-console.log(bubble);
+// console.log('After bubbleSort');
+// console.log(bubble);
 
 
 // SelectionSort
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function selectionSort(array) {
-    
+    let length = array.length;
+    for (let i = 0; i < length; i++){
+        let minIdx = i;
+        let tmp = array[i];
+        for (let j = i+1; j < length; j++){
+            if (array[j]<array[minIdx]) {
+                minIdx = j;
+            }
+        }
+      
+        array[i] = array[minIdx];
+        array[minIdx] = tmp;
+    }
 }
+
 console.log('Before selectionSort');
 console.log(numbers);
 
@@ -50,3 +63,18 @@ selectionSort(numbers);
 
 console.log('After selectionSort');
 console.log(numbers);
+
+
+const numbers2 = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array) {
+  
+}
+
+
+console.log('Before insertionSort');
+console.log(numbers2);
+
+insertionSort(numbers2);
+console.log('After insertionSort');
+console.log(numbers2);
