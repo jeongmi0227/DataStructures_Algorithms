eab0efb53cd7a416bb899502346565b343c6d4ed
Jeong Mi Park <parkjeongmi0227@gmail.com>
2022-08-21 23:45:18 -0400

    Implmented searching algorithm with Breadth First Search

--- Algorithm/Searching/searching_intro.js
+++ Algorithm/Searching/searching_intro.js
@@ -34,16 +34,260 @@
 // Depth First Search has a lower memory requirement than breadth First Search, because it's not necessary to store all the child pointers at each level.
 
 
-// Props and cons of DFS / DFS
+// Props and cons of BFS / DFS
+// They both do the same thing traversal, but each one is used ofr different reasons and in interviews
+// you'll get questions about what type of traversal to do and usually you'll have one of these two answers
+// Time complexity for BFS and DFS are same
+// They all visit the nodes at lease once, so it is O(n) all traversal is O(n)
+// When we are trying to traverse through a tree or a graph we are really trying to do is walk through 
+// the tree withough ever repeating ourselves.
+// The order is the thing that matters when it comes to BFS, DFS for a search
+
+
+// BFS
+// Adavantage: It's very good for finding the shortest path between a starting point and any other reachable node.
+// Because we always start off with the root node and then search the cloest nodes first and then the nodes further and then further.
+// Disadvantage: BFS requires more memory than DFS
+
+// If you have additional information on the location of the target node.
+// You know that the node is likely in the upper level of a graph, then BFS is better.
+// (Because it will search through the cloest nodes first.)
+
+
+// DFS
+// If you know that the node is likely at the lower level of a graph, 
+// Perhaps depth where search is better in that case (opposite to BFS)
+
+// Advantage: Good at asking the question does the path exist to a certain node from a source node to a target node
+// It uses less memory than BFS
+// Disadvantage: It can get slow, if the tree or graph is really deep and it's not necessarily good at finding the shortest path like BFS
+
+
 
 //If you know a solution is not far from the root of the tree:
+// BFS
 
 //If the tree is very deep and solutions are rare: 
+// BFS (DFS will take long)
 
 //If the tree is very wide:
+// DFS (BFS will need too much memory)
+// The way BFS works is that it has to keep track of the nodes
+// in the current level in a queue (might take up too much memory)
 
 //If solutions are frequent but located deep in the tree:
+// DFS
 
 //Determining whether a path exists between two nodes:
+// DFS
 
 //Finding the shortest path:
+// BFS
+class Node {
+    constructor(value) {
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+class BinarySearchTree {
+    
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            let currentNode = this.root;
+            while (true) {
+                if (value < currentNode.value) {
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+    }
+
+    lookup(value) {
+        if (!this.root) {
+            return false;
+        }
+
+        let currentNode = this.root;
+
+        while (currentNode) {
+            if (currentNode.value > value) {
+                currentNode = currentNode.left;
+            } else if (currentNode.value < value) {
+                currentNode = currentNode.right;
+            } else if (currentNode.value === value) {
+                return currentNode;
+            }
+        }
+        return false;
+    }
+
+    // remove
+    remove2(value) {
+
+        let currentNode = this.lookup(value);
+        let newNode = this.lookup(value);
+
+        currentNode = currentNode.right;
+       
+        while (currentNode.left != null) {
+            currentNode = currentNode.left;
+        }
+        newNode.value = currentNode.value;
+        currentNode.value = null;
+
+        return newNode;
+    }
+
+
+    remove(value) {
+        if (!this.root) {
+            return false;
+        }
+        let currentNode = this.root;
+        let parentNode = null;
+        while (currentNode) {
+            if (value < currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            } else if (currentNode.value === value) {
+                //We have a match, get to work!
+            
+                //Option 1: No right child: 
+                if (currentNode.right === null) {
+                    if (parentNode === null) {
+                        this.root = currentNode.left;
+                    } else {
+                
+                        //if parent > current value, make current left child a child of parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.left;
+                
+                            //if parent < current value, make left child a right child of parent
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.left;
+                        }
+                    }
+            
+                    //Option 2: Right child which doesnt have a left child
+                } else if (currentNode.right.left === null) {
+                    currentNode.right.left = currentNode.left;
+                    if (parentNode === null) {
+                        this.root = currentNode.right;
+                    } else {
+                
+                        //if parent > current, make right child of the left the parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.right;
+                
+                            //if parent < current, make right child a right child of the parent
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.right;
+                        }
+                    }
+            
+                    //Option 3: Right child that has a left child
+                } else {
+    
+                    //find the Right child's left most child
+                    let leftmost = currentNode.right.left;
+                    let leftmostParent = currentNode.right;
+                    while (leftmost.left !== null) {
+                        leftmostParent = leftmost;
+                        leftmost = leftmost.left;
+                    }
+              
+                    //Parent's left subtree is now leftmost's right subtree
+                    leftmostParent.left = leftmost.right;
+                    leftmost.left = currentNode.left;
+                    leftmost.right = currentNode.right;
+    
+                    if (parentNode === null) {
+                        this.root = leftmost;
+                    } else {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = leftmost;
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = leftmost;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+    }
+    // Iterative
+    breadthFirstSearch() {
+        let currentNode = this.root;
+        let list = [];
+        let queue = [];
+        queue.push(currentNode);
+    
+        while (queue.length > 0) {
+            currentNode = queue.shift();
+            console.log(currentNode.value);
+            list.push(currentNode.value);
+            if (currentNode.left) {
+                queue.push(currentNode.left);
+            }
+            if (currentNode.right) {
+                queue.push(currentNode.right);
+            }
+        }
+        return list;
+    }
+    // Recursive
+    breadthFirstSearchR(queue,list) {
+        if (!queue.length) {
+            return list;
+        }
+        const currentNode = queue.shift();
+        list.push(currentNode.value);
+        if (currentNode.left) {
+            queue.push(currentNode.left);
+        }
+        if (currentNode.right) {
+            queue.push(currentNode.right);
+        }
+
+        return this.breadthFirstSearchR(queue, list);
+
+    }
+}
+    
+
+const tree = new BinarySearchTree();
+
+tree.insert(9);
+
+
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(tree.breadthFirstSearch());
+
+console.log(tree.breadthFirstSearch([tree.root],[]));
+
--- Algorithm/Sorting/Sorting_interview.js
+++ Algorithm/Sorting/Sorting_interview.js
@@ -1,15 +1,56 @@
 //#1 - Sort 10 schools around your house by distance:
 // Quick sort - small lists of items
+// Insertion sort : Easy to code and it has space complexity of O(1)
+
 //#2 - eBay sorts listings by the current Bid amount:
 // Merge sort 
+// Radix or Counting sort : Bid is Fixed length of intergers O(n logN)ß
+
 //#3 - Sport scores on ESPN
 // Counting sort
+// Quick sort :  better space for complexity
+
 //#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+// Merge sort: Data is big O(n logN)
 
 //#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+// Counting sort
+// Insertion sort : pre ordered list is going to work better than any other type of sorting.
 
 //#6 - Temperature Records for the past 50 years in Canada
+// Counting sort
+// Radix or Counting (if tmp no decimal places)
+// Quick sort (Decimal numbers)
 
 //#7 - Large user name database needs to be sorted. Data is very random.
+// Quick or Merge
+// Quick sort
 
 //#8 - You want to teach sorting for the first time
+// Bubble sort, selection sort
+
+// In most cases, sorting algorithm that the languages using underneath the hood is 
+// either quick sort or insertion sort and merge sort combine together.
+
+
+// Review
+// Quick sort: Use quick sort when the average case performance matters more than the worst case performances
+// Because quick sort allows us to sort in O(n logN) but worst case in O(n^2)
+// Merge sort: Always O(n logN), it is also stable, gives same results in order, if two items are the same.
+// Although the space complexity is a little bit hibher than quick sort , it is definitely a useful sort.
+
+
+// Insertion sort, selection sort, bubble sort : Doing something quick and dirty and experiment arount sorting.
+// (Although insertion sort is great for small or nearly sorted inputs)
+
+// Interview tips!
+// During the interview you might say, in those rare cases that they ask you to implment a sorting algorithm, you'll say 
+// well, because we have a time constraint, I would like to use something naive and not efficient, like bubble sort, which is slow in real life.
+// Instead of implementing something like merge sort, if it's okay with you becuase we can assume that merge sort is something that we can implement with a library or fairly easily with documentation. 
+
+// Or, you can say, we can just use Merge sort to divide and conquer the code to make it more efficient
+
+// Important!
+// Understand the trade offs between different algorithms
+// Decide based on their speed, stability and their readability whether the input is already sorted or nearly sorted
+// How large is the input, we can make decision on sorting.
