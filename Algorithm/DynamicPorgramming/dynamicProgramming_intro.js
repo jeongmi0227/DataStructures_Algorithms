393a83dade866f9a97e604cd600d4da9eb240636
Jeong Mi Park <parkjeongmi0227@gmail.com>
2022-11-03 21:06:05 -0400

    Fibonacci and Dynamic Programming

--- Algorithm/DynamicPorgramming/dynamicProgramming_intro.js
+++ Algorithm/DynamicPorgramming/dynamicProgramming_intro.js
@@ -8,6 +8,14 @@
  * 
  * Memorization is a specific form of caching that invovles caching the return value.
  * memorization is simply a way to remember a solution to a problem.
+ * 
+ * Dynamic Programming as combining divide & conquer and memoization with recursion to get savings in performance.
+ * 
+ * 1. Can be divided into subproblem?
+ * 2. Recursive solution
+ * 3. Are there repetitive subproblems?
+ * 4. Memoize subproblems
+ * 5. increase the performance
  */
 function addTo80(n) {
     console.log('Long time');
@@ -29,7 +37,47 @@ function memoizedAddTo80() {
     }
 }
 
-const memized = memoizedAddTo80();
-console.log('1',memized(5));
-console.log('2', memized(5));
-console.log('3',memized(6));
+// const memized = memoizedAddTo80();
+// console.log('1',memized(5));
+// console.log('2', memized(5));
+// console.log('3', memized(6));
+
+// fibonacci recursion takes O(2^n)
+// Dynamic function can reduce time and space complexity O(n) by using memroziation
+let cacluations1 = 0;
+function fibonacci(n) {
+    cacluations1++;
+    
+    if (n < 2) {
+        return n;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
+let cacluations2 = 0;
+function fibonacciMaster() {
+    // Time complexity O(n) downside is increased space complexity
+    // Hash table or an object to store pre calculated answers.
+    let cache = {};
+    return function fib(n) {
+        cacluations2++;
+        if (n in cache) {
+            return cache[n];
+        } else {
+            if (n < 2) {
+                return n;
+            }
+            cache[n] = fib(n - 1) + fib(n - 2);
+            return cache[n];
+        }
+    }
+}
+
+console.log('fib ',fibonacci(35)); 
+console.log('cacluations1 ',cacluations1);
+
+const fasterFib = fibonacciMaster();
+console.log('DP ', fasterFib(35));
+console.log('cacluations2 ', cacluations2);
